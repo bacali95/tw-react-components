0bad4455a5083912a242d2ae93c9084155e5a14d
Nasreddine Bac Ali <nasreddine.bacali95@gmail.com>
2023-03-09 09:35:50 +0100

    feat(block): add forwarding ref

--- libs/tw-react-components/src/components/Block/index.tsx
+++ libs/tw-react-components/src/components/Block/index.tsx
@@ -1,33 +1,30 @@
 import classNames from 'classnames';
-import { ComponentProps, FC, PropsWithChildren } from 'react';
+import { ComponentProps, PropsWithoutRef, forwardRef } from 'react';
 
-export type BlockProps = ComponentProps<'div'> & {
-  className?: string;
-  centered?: boolean;
-  container?: boolean;
-  fullWidth?: boolean;
-  fullHeight?: boolean;
-};
+export type BlockProps = PropsWithoutRef<
+  ComponentProps<'div'> & {
+    className?: string;
+    centered?: boolean;
+    container?: boolean;
+    fullWidth?: boolean;
+    fullHeight?: boolean;
+  }
+>;
 
-export const Block: FC<PropsWithChildren<BlockProps>> = ({
-  children,
-  className,
-  centered,
-  container,
-  fullWidth,
-  fullHeight,
-  ...props
-}) => (
-  <div
-    className={classNames(
-      className,
-      centered && 'mx-auto',
-      container && 'container',
-      fullWidth && 'w-full',
-      fullHeight && 'h-full'
-    )}
-    {...props}
-  >
-    {children}
-  </div>
+export const Block = forwardRef<HTMLDivElement, BlockProps>(
+  ({ children, className, centered, container, fullWidth, fullHeight, ...props }, ref) => (
+    <div
+      className={classNames(
+        className,
+        centered && 'mx-auto',
+        container && 'container',
+        fullWidth && 'w-full',
+        fullHeight && 'h-full'
+      )}
+      {...props}
+      ref={ref}
+    >
+      {children}
+    </div>
+  )
 );
