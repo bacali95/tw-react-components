7ec349d3c49c4849c5bf51cca0f9ed223659c98d
Nasreddine Bac Ali <nasreddine.bacali95@gmail.com>
2023-04-10 16:41:42 +0200

    fix(navbar): fix styling

--- libs/tw-react-components/src/components/Navbar/index.tsx
+++ libs/tw-react-components/src/components/Navbar/index.tsx
@@ -2,14 +2,15 @@ import { MenuIcon } from 'lucide-react';
 import { FC, PropsWithChildren } from 'react';
 
 import { useLayoutContext } from '../../contexts';
+import { Button } from '../Button';
 
 export const Navbar: FC<PropsWithChildren> = ({ children }) => {
   const { toggleSidebar } = useLayoutContext();
 
   return (
     <div className="p-1">
-      <div className="flex w-full select-none items-center justify-between rounded-lg bg-white p-1 px-3 text-black shadow dark:bg-slate-800 dark:text-white">
-        <MenuIcon className="h-6 w-6 cursor-pointer" onClick={toggleSidebar} />
+      <div className="flex w-full select-none items-center justify-between rounded-lg bg-white p-2 text-black shadow dark:bg-slate-800 dark:text-white">
+        <Button prefixIcon={MenuIcon} transparent onClick={toggleSidebar} />
         <div className="flex items-center gap-2 py-1">{children}</div>
       </div>
     </div>
