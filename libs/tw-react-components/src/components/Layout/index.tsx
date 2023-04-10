f0825df724f14c01dbb8ae5fdccf021259ef6f77
Nasreddine Bac Ali <nasreddine.bacali95@gmail.com>
2023-04-10 22:36:19 +0200

    fix(layout): remove theme switcher from navbar

--- libs/tw-react-components/src/components/Layout/index.tsx
+++ libs/tw-react-components/src/components/Layout/index.tsx
@@ -3,7 +3,6 @@ import { FC, PropsWithChildren, ReactNode } from 'react';
 import { Flex } from '../Flex';
 import { Navbar } from '../Navbar';
 import { Sidebar, SidebarProps } from '../Sidebar';
-import { ThemeSwitcher } from '../ThemeSwitcher';
 
 type Props = {
   sidebarProps: SidebarProps;
@@ -19,10 +18,7 @@ export const Layout: FC<PropsWithChildren<Props>> = ({
     <div className="flex h-screen gap-1 bg-slate-100 p-2 dark:bg-slate-900">
       <Sidebar {...sidebarProps} />
       <div className="flex w-full flex-col gap-1 overflow-hidden">
-        <Navbar>
-          {navbarChildren}
-          <ThemeSwitcher />
-        </Navbar>
+        <Navbar>{navbarChildren}</Navbar>
         <Flex className="overflow-hidden p-1" direction="column" fullWidth fullHeight>
           {children}
         </Flex>
