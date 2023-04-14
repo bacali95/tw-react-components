3ad3a442afc657fa55b3f4313d7e4c41c338848f
Nasreddine Bac Ali <nasreddine.bacali95@gmail.com>
2023-04-14 14:35:15 +0200

    fix(form-group): fix styling

--- libs/tw-react-components/src/components/Form/controls/FormGroup.tsx
+++ libs/tw-react-components/src/components/Form/controls/FormGroup.tsx
@@ -1,14 +1,22 @@
+import classNames from 'classnames';
 import { FC, PropsWithChildren } from 'react';
 
-type Props = PropsWithChildren<{
+import { Flex } from '../../Flex';
+
+export type FormGroupProps = PropsWithChildren<{
+  className?: string;
   label: string;
 }>;
 
-export const FormGroup: FC<Props> = ({ label, children }) => (
-  <div className="relative flex flex-col gap-4 rounded-lg border p-4 dark:border-slate-300">
-    <div className="absolute right-0 top-0 rounded-bl-lg rounded-tr-lg bg-slate-500 px-2 py-1 font-medium text-white dark:bg-slate-300 dark:bg-slate-900 dark:text-slate-400">
+export const FormGroup: FC<FormGroupProps> = ({ className, label, children }) => (
+  <Flex
+    className={classNames('relative !gap-4 rounded-lg border p-4 dark:border-slate-300', className)}
+    direction="column"
+    fullWidth
+  >
+    <div className="absolute right-0 top-0 rounded-bl-lg rounded-tr-lg bg-slate-500 px-2 py-1 font-medium text-white dark:bg-slate-900 dark:text-white">
       {label}
     </div>
     {children}
-  </div>
+  </Flex>
 );
