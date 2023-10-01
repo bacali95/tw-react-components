d76042a5c76e49114a5604b71693d9b163eee200
Nasreddine Bac Ali <nasreddine.bacali95@gmail.com>
2023-10-01 07:22:15 +0000

    feat: allow sidebar items to be hidden

--- libs/tw-react-components/src/components/Sidebar/index.tsx
+++ libs/tw-react-components/src/components/Sidebar/index.tsx
@@ -13,6 +13,7 @@ export type SidebarItem = {
   title: string;
   label?: ReactNode;
   Icon: LucideIcon;
+  hidden?: boolean;
   items?: SidebarItem[];
 };
 
@@ -57,49 +58,53 @@ export const Sidebar: FC<SidebarProps> = ({ items, smallLogo, fullLogo }) => {
             </Link>
           </div>
           <Accordion.Root className="overflow-hidden" type="single">
-            {items.map((item) => (
-              <Accordion.Item
-                key={item.pathname}
-                className={classNames('flex flex-col rounded-md', {
-                  'bg-slate-100 dark:bg-slate-900':
-                    item.items &&
-                    (currentTab === item.pathname ||
-                      item.items.some((subItem) => currentTab === subItem.pathname)),
-                })}
-                value={item.pathname}
-              >
-                <Accordion.Header>
-                  <Accordion.Trigger className="relative w-full p-1 data-[state=open]:[--rotate-chevron:90deg]">
-                    <SidebarItemComp
-                      {...item}
-                      active={currentTab === item.pathname}
-                      sidebarOpen={sidebarOpen}
-                      onClick={onLinkClick}
-                    />
+            {items.map((item) =>
+              item.hidden ? undefined : (
+                <Accordion.Item
+                  key={item.pathname}
+                  className={classNames('flex flex-col rounded-md', {
+                    'bg-slate-100 dark:bg-slate-900':
+                      item.items &&
+                      (currentTab === item.pathname ||
+                        item.items.some((subItem) => currentTab === subItem.pathname)),
+                  })}
+                  value={item.pathname}
+                >
+                  <Accordion.Header>
+                    <Accordion.Trigger className="relative w-full p-1 data-[state=open]:[--rotate-chevron:90deg]">
+                      <SidebarItemComp
+                        {...item}
+                        active={currentTab === item.pathname}
+                        sidebarOpen={sidebarOpen}
+                        onClick={onLinkClick}
+                      />
 
-                    {sidebarOpen && item.items && (
-                      <ChevronRightIcon className="absolute right-2 top-1/2 h-4 w-4 -translate-y-1/2 rotate-[var(--rotate-chevron,0deg)] transition-transform duration-200" />
-                    )}
-                  </Accordion.Trigger>
-                </Accordion.Header>
-                {item.items && (
-                  <Accordion.Content className="overflow-hidden data-[state=closed]:animate-[slideUp_200ms_ease-out] data-[state=open]:animate-[slideDown_200ms_ease-out]">
-                    <div className="flex flex-col gap-1 p-1 pt-0">
-                      {item.items.map((subItem) => (
-                        <SidebarItemComp
-                          key={subItem.pathname}
-                          {...subItem}
-                          isChild
-                          active={currentTab === subItem.pathname}
-                          sidebarOpen={sidebarOpen}
-                          onClick={onLinkClick}
-                        />
-                      ))}
-                    </div>
-                  </Accordion.Content>
-                )}
-              </Accordion.Item>
-            ))}
+                      {sidebarOpen && item.items && (
+                        <ChevronRightIcon className="absolute right-2 top-1/2 h-4 w-4 -translate-y-1/2 rotate-[var(--rotate-chevron,0deg)] transition-transform duration-200" />
+                      )}
+                    </Accordion.Trigger>
+                  </Accordion.Header>
+                  {item.items && (
+                    <Accordion.Content className="overflow-hidden data-[state=closed]:animate-[slideUp_200ms_ease-out] data-[state=open]:animate-[slideDown_200ms_ease-out]">
+                      <div className="flex flex-col gap-1 p-1 pt-0">
+                        {item.items.map((subItem) =>
+                          subItem.hidden ? undefined : (
+                            <SidebarItemComp
+                              key={subItem.pathname}
+                              {...subItem}
+                              isChild
+                              active={currentTab === subItem.pathname}
+                              sidebarOpen={sidebarOpen}
+                              onClick={onLinkClick}
+                            />
+                          )
+                        )}
+                      </div>
+                    </Accordion.Content>
+                  )}
+                </Accordion.Item>
+              )
+            )}
           </Accordion.Root>
         </div>
       </div>
