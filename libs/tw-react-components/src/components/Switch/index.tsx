cea45c59a3225b06f05bbc32f81fc9f722b4e810
Nasreddine Bac Ali <nasreddine.bacali95@gmail.com>
2023-11-06 21:59:48 +0000

    fix: fix switch style

--- libs/tw-react-components/src/components/Switch/index.tsx
+++ libs/tw-react-components/src/components/Switch/index.tsx
@@ -8,7 +8,7 @@ const Switch = forwardRef<
 >(({ className, ...props }, ref) => (
   <SwitchPrimitives.Root
     className={classNames(
-      'peer inline-flex h-[24px] w-[44px] shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent',
+      'peer inline-flex h-[calc(1.5rem+6px)] w-[calc(3rem+6px)] shrink-0 cursor-pointer items-center rounded-full border-[3px] border-transparent',
       'transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-600 focus-visible:ring-offset-2',
       'focus-visible:ring-offset-white disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-blue-600 data-[state=unchecked]:bg-slate-300',
       'dark:focus-visible:ring-slate-200 dark:focus-visible:ring-offset-slate-800 dark:data-[state=checked]:bg-blue-700 dark:data-[state=unchecked]:bg-slate-700',
@@ -19,8 +19,8 @@ const Switch = forwardRef<
   >
     <SwitchPrimitives.Thumb
       className={classNames(
-        'pointer-events-none block h-5 w-5 rounded-full bg-white shadow-lg ring-0 transition-transform',
-        'data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0 dark:bg-slate-900'
+        'pointer-events-none block h-6 w-6 rounded-full bg-white shadow-lg ring-0 transition-transform',
+        'data-[state=checked]:translate-x-6 data-[state=unchecked]:translate-x-0 dark:bg-slate-900'
       )}
     />
   </SwitchPrimitives.Root>
