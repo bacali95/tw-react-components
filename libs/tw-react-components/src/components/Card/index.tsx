1a5258a8051850e084432d5ac384a1e41ccb7601
Nasreddine Bac Ali <nasreddine.bacali@uber.com>
2024-01-13 20:43:05 +0000

    fix: forward ref fo card component

--- libs/tw-react-components/src/components/Card/index.tsx
+++ libs/tw-react-components/src/components/Card/index.tsx
@@ -1,16 +1,19 @@
 import classNames from 'classnames';
-import { FC, PropsWithChildren } from 'react';
+import { forwardRef } from 'react';
 
 import { Block, BlockProps } from '../Block';
 
-export const Card: FC<PropsWithChildren<BlockProps>> = ({ children, className, ...blockProps }) => (
-  <Block
-    className={classNames(
-      className,
-      'rounded-lg bg-white p-3 shadow dark:bg-slate-800 dark:text-white'
-    )}
-    {...blockProps}
-  >
-    {children}
-  </Block>
+export const Card = forwardRef<HTMLDivElement, BlockProps>(
+  ({ children, className, ...blockProps }, ref) => (
+    <Block
+      className={classNames(
+        className,
+        'rounded-lg bg-white p-3 shadow dark:bg-slate-800 dark:text-white'
+      )}
+      {...blockProps}
+      ref={ref}
+    >
+      {children}
+    </Block>
+  )
 );
