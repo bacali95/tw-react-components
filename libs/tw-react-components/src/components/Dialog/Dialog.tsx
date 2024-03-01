8ca8c6ced3debef5087d2c1ddbfe5a2bdf8b6746
Nasreddine Bac Ali <nasreddine.bacali95@gmail.com>
2024-03-01 11:20:20 +0100

    fix: fix dialog small screens design

--- libs/tw-react-components/src/components/Dialog/Dialog.tsx
+++ libs/tw-react-components/src/components/Dialog/Dialog.tsx
@@ -38,7 +38,7 @@ const DialogContent = forwardRef<
       ref={ref}
       className={cn(
         'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]',
-        'fixed left-[50%] top-[50%] z-50 flex w-full max-w-lg translate-x-[-50%] translate-y-[-50%] flex-col gap-4 rounded-lg bg-white p-4 shadow-lg duration-200 dark:bg-slate-800 dark:text-white',
+        'fixed left-[50%] top-[50%] z-50 flex w-full max-w-lg translate-x-[-50%] translate-y-[-50%] flex-col gap-3 rounded-lg bg-white p-3 shadow-lg duration-200 xl:gap-4 xl:p-4 dark:bg-slate-800 dark:text-white',
         className,
         fullScreen && 'h-full max-h-none w-full max-w-none rounded-none'
       )}
@@ -46,7 +46,7 @@ const DialogContent = forwardRef<
     >
       {children}
       <DialogPrimitive.Close
-        className="!absolute right-4 top-4 opacity-70 ring-offset-white transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 disabled:pointer-events-none"
+        className="!absolute right-3 top-3 opacity-70 ring-offset-white transition-opacity hover:opacity-100 disabled:pointer-events-none xl:right-4 xl:top-4"
         asChild
       >
         <Button prefixIcon={XIcon} size="small">
