e288236e9f5e2a34d8c6b6813e300a5700b13f81
Nasreddine Bac Ali <nasreddine.bacali95@gmail.com>
2024-09-10 10:57:45 +0000

    chore: update dependencies

--- libs/tw-react-components/src/components/Form/controls/with-form.tsx
+++ libs/tw-react-components/src/components/Form/controls/with-form.tsx
@@ -55,37 +55,36 @@ function withForm<
       ? SelectInputProps
       : BasicInputProps<Type>,
 >(Component: ForwardRefExoticComponent<Props>) {
-  return forwardRef<HtmlElement, WithFormProps<Type, Props>>(
-    ({ name, pattern, validate, ...props }, ref) => {
-      const { control } = useFormContext();
+  return forwardRef<HtmlElement, WithFormProps<Type, Props>>((props, ref) => {
+    const { name, pattern, validate, ...restProps } = props as WithFormProps<Type, Props>;
+    const { control } = useFormContext();
 
-      return (
-        <Controller
-          name={name}
-          control={control}
-          rules={{
-            required: (props as ComponentProps<'input'>).required,
-            min: (props as ComponentProps<'input'>).min,
-            max: (props as ComponentProps<'input'>).max,
-            minLength: (props as ComponentProps<'input'>).minLength,
-            maxLength: (props as ComponentProps<'input'>).maxLength,
-            pattern,
-            validate,
-          }}
-          render={({ field, fieldState }) => (
-            <Component
-              {...(props as Props)}
-              {...field}
-              value={field.value ?? ''}
-              disabled={field.disabled ?? (props as ComponentProps<'input'>).disabled}
-              hasErrors={fieldState.error}
-              ref={mergeRefs([ref, field.ref])}
-            />
-          )}
-        />
-      );
-    },
-  );
+    return (
+      <Controller
+        name={name}
+        control={control}
+        rules={{
+          required: (restProps as ComponentProps<'input'>).required,
+          min: (restProps as ComponentProps<'input'>).min,
+          max: (restProps as ComponentProps<'input'>).max,
+          minLength: (restProps as ComponentProps<'input'>).minLength,
+          maxLength: (restProps as ComponentProps<'input'>).maxLength,
+          pattern,
+          validate,
+        }}
+        render={({ field, fieldState }) => (
+          <Component
+            {...(restProps as Props)}
+            {...field}
+            value={field.value ?? ''}
+            disabled={field.disabled ?? (restProps as ComponentProps<'input'>).disabled}
+            hasErrors={fieldState.error}
+            ref={mergeRefs([ref, field.ref])}
+          />
+        )}
+      />
+    );
+  });
 }
 
 export const FormInputs = {
