ebd5c58670c360594eec5d2fc710db6be7000e01
Nasreddine Bac Ali <nasreddine.bacali95@gmail.com>
2025-12-21 11:41:49 +0000

    chore: upgrade Yarn to version 4.12.0, update dependencies, and refactor Resizable component structure

--- apps/demo/src/pages/Resizable.tsx
+++ apps/demo/src/pages/Resizable.tsx
@@ -5,8 +5,8 @@ import { Flex, Resizable } from 'tw-react-components';
 export const ResizablePage: FC = () => {
   return (
     <Flex fullHeight fullWidth align="center" justify="center">
-      <Resizable.PanelGroup
-        direction="horizontal"
+      <Resizable.Group
+        orientation="horizontal"
         className="max-w-md rounded-lg border md:max-h-[400px] md:min-w-[450px]"
       >
         <Resizable.Panel defaultSize={50}>
@@ -14,23 +14,23 @@ export const ResizablePage: FC = () => {
             <span className="font-semibold">One</span>
           </div>
         </Resizable.Panel>
-        <Resizable.Handle withHandle />
+        <Resizable.Separator withHandle />
         <Resizable.Panel defaultSize={50}>
-          <Resizable.PanelGroup direction="vertical">
+          <Resizable.Group orientation="vertical" className="flex flex-col">
             <Resizable.Panel defaultSize={25}>
               <div className="flex h-full items-center justify-center p-6">
                 <span className="font-semibold">Two</span>
               </div>
             </Resizable.Panel>
-            <Resizable.Handle withHandle />
+            <Resizable.Separator withHandle />
             <Resizable.Panel defaultSize={75}>
               <div className="flex h-full items-center justify-center p-6">
                 <span className="font-semibold">Three</span>
               </div>
             </Resizable.Panel>
-          </Resizable.PanelGroup>
+          </Resizable.Group>
         </Resizable.Panel>
-      </Resizable.PanelGroup>
+      </Resizable.Group>
     </Flex>
   );
 };
--- libs/tw-react-components/src/components/Resizable/index.tsx
+++ libs/tw-react-components/src/components/Resizable/index.tsx
@@ -4,17 +4,17 @@ import * as ResizablePrimitive from 'react-resizable-panels';
 
 import { cn } from '../../helpers';
 
-export type ResizablePanelGroupProps = React.ComponentProps<
-  typeof ResizablePrimitive.PanelGroup
-> & { dataTestId?: string };
+export type ResizableGroupProps = React.ComponentProps<typeof ResizablePrimitive.Group> & {
+  dataTestId?: string;
+};
 
-const ResizablePanelGroup: FC<ResizablePanelGroupProps> = ({
+const ResizableGroup: FC<ResizableGroupProps> = ({
   className,
-  dataTestId = 'resizable-panel-group',
+  dataTestId = 'resizable-group',
   ...props
 }) => (
-  <ResizablePrimitive.PanelGroup
-    className={cn('flex h-full w-full data-[panel-group-direction=vertical]:flex-col', className)}
+  <ResizablePrimitive.Group
+    className={cn('flex h-full w-full', className)}
     data-testid={dataTestId}
     {...props}
   />
@@ -28,23 +28,21 @@ const ResizablePanel: FC<ResizablePanelProps> = ({ dataTestId = 'resizable-panel
   <ResizablePrimitive.Panel data-testid={dataTestId} {...props} />
 );
 
-export type ResizableHandleProps = React.ComponentProps<
-  typeof ResizablePrimitive.PanelResizeHandle
-> & {
+export type ResizableSeparatorProps = React.ComponentProps<typeof ResizablePrimitive.Separator> & {
   withHandle?: boolean;
   dataTestId?: string;
 };
 
-const ResizableHandle: FC<ResizableHandleProps> = ({
+const ResizableSeparator: FC<ResizableSeparatorProps> = ({
   withHandle,
   className,
   dataTestId = 'resizable-handle',
   ...props
 }) => (
-  <ResizablePrimitive.PanelResizeHandle
+  <ResizablePrimitive.Separator
     className={cn(
       'bg-border focus-visible:ring-ring relative flex w-px items-center justify-center after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:ring-1 focus-visible:ring-offset-1 focus-visible:outline-hidden',
-      'data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:translate-x-0 data-[panel-group-direction=vertical]:after:-translate-y-1/2 [&[data-panel-group-direction=vertical]>div]:rotate-90',
+      'aria-[orientation=horizontal]:h-px aria-[orientation=horizontal]:w-full aria-[orientation=horizontal]:after:left-0 aria-[orientation=horizontal]:after:h-1 aria-[orientation=horizontal]:after:w-full aria-[orientation=horizontal]:after:translate-x-0 aria-[orientation=horizontal]:after:-translate-y-1/2 [&[aria-orientation=horizontal]>div]:rotate-90',
       className,
     )}
     data-testid={dataTestId}
@@ -58,11 +56,11 @@ const ResizableHandle: FC<ResizableHandleProps> = ({
         <GripVertical className="h-2.5 w-2.5" />
       </div>
     )}
-  </ResizablePrimitive.PanelResizeHandle>
+  </ResizablePrimitive.Separator>
 );
 
 export const Resizable = {
-  PanelGroup: ResizablePanelGroup,
+  Group: ResizableGroup,
   Panel: ResizablePanel,
-  Handle: ResizableHandle,
+  Separator: ResizableSeparator,
 };
